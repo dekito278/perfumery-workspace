99ea1b93a74190b69c5e45b181904f76dcbd89ac
dekito278 <aderizki68@gmail.com>
2026-05-09 11:42:20 +0700

    fix ios formula pdf import

--- apps/web/src/utils/polyfills.js
+++ apps/web/src/utils/polyfills.js
@@ -131,3 +131,117 @@ if (typeof Response !== 'undefined' && !Response.prototype.bytes) {
     writable: true,
   });
 }
+
+if (typeof globalThis.DOMMatrix === 'undefined') {
+  class DOMMatrixPolyfill {
+    constructor(init) {
+      const values = Array.isArray(init) || ArrayBuffer.isView(init) ? Array.from(init) : null;
+      this.a = Number(values?.[0] ?? 1);
+      this.b = Number(values?.[1] ?? 0);
+      this.c = Number(values?.[2] ?? 0);
+      this.d = Number(values?.[3] ?? 1);
+      this.e = Number(values?.[4] ?? 0);
+      this.f = Number(values?.[5] ?? 0);
+      this.m11 = this.a;
+      this.m12 = this.b;
+      this.m21 = this.c;
+      this.m22 = this.d;
+      this.m41 = this.e;
+      this.m42 = this.f;
+      this.is2D = true;
+      this.isIdentity = this.a === 1 && this.b === 0 && this.c === 0 && this.d === 1 && this.e === 0 && this.f === 0;
+    }
+
+    _sync() {
+      this.m11 = this.a;
+      this.m12 = this.b;
+      this.m21 = this.c;
+      this.m22 = this.d;
+      this.m41 = this.e;
+      this.m42 = this.f;
+      this.isIdentity = this.a === 1 && this.b === 0 && this.c === 0 && this.d === 1 && this.e === 0 && this.f === 0;
+      return this;
+    }
+
+    multiplySelf(other = new DOMMatrixPolyfill()) {
+      const matrix = other instanceof DOMMatrixPolyfill ? other : new DOMMatrixPolyfill(other);
+      const { a, b, c, d, e, f } = this;
+      this.a = a * matrix.a + c * matrix.b;
+      this.b = b * matrix.a + d * matrix.b;
+      this.c = a * matrix.c + c * matrix.d;
+      this.d = b * matrix.c + d * matrix.d;
+      this.e = a * matrix.e + c * matrix.f + e;
+      this.f = b * matrix.e + d * matrix.f + f;
+      return this._sync();
+    }
+
+    preMultiplySelf(other = new DOMMatrixPolyfill()) {
+      const matrix = other instanceof DOMMatrixPolyfill ? other : new DOMMatrixPolyfill(other);
+      const current = new DOMMatrixPolyfill([this.a, this.b, this.c, this.d, this.e, this.f]);
+      this.a = matrix.a;
+      this.b = matrix.b;
+      this.c = matrix.c;
+      this.d = matrix.d;
+      this.e = matrix.e;
+      this.f = matrix.f;
+      return this.multiplySelf(current);
+    }
+
+    multiply(other) {
+      return new DOMMatrixPolyfill([this.a, this.b, this.c, this.d, this.e, this.f]).multiplySelf(other);
+    }
+
+    translateSelf(x = 0, y = 0) {
+      return this.multiplySelf(new DOMMatrixPolyfill([1, 0, 0, 1, Number(x) || 0, Number(y) || 0]));
+    }
+
+    translate(x = 0, y = 0) {
+      return new DOMMatrixPolyfill([this.a, this.b, this.c, this.d, this.e, this.f]).translateSelf(x, y);
+    }
+
+    scaleSelf(scaleX = 1, scaleY = scaleX) {
+      return this.multiplySelf(new DOMMatrixPolyfill([Number(scaleX) || 0, 0, 0, Number(scaleY) || 0, 0, 0]));
+    }
+
+    scale(scaleX = 1, scaleY = scaleX) {
+      return new DOMMatrixPolyfill([this.a, this.b, this.c, this.d, this.e, this.f]).scaleSelf(scaleX, scaleY);
+    }
+
+    invertSelf() {
+      const determinant = this.a * this.d - this.b * this.c;
+      if (!determinant) {
+        this.a = Number.NaN;
+        this.b = Number.NaN;
+        this.c = Number.NaN;
+        this.d = Number.NaN;
+        this.e = Number.NaN;
+        this.f = Number.NaN;
+        return this._sync();
+      }
+
+      const { a, b, c, d, e, f } = this;
+      this.a = d / determinant;
+      this.b = -b / determinant;
+      this.c = -c / determinant;
+      this.d = a / determinant;
+      this.e = (c * f - d * e) / determinant;
+      this.f = (b * e - a * f) / determinant;
+      return this._sync();
+    }
+
+    inverse() {
+      return new DOMMatrixPolyfill([this.a, this.b, this.c, this.d, this.e, this.f]).invertSelf();
+    }
+
+    toFloat32Array() {
+      return new Float32Array([this.a, this.b, 0, 0, this.c, this.d, 0, 0, 0, 0, 1, 0, this.e, this.f, 0, 1]);
+    }
+
+    toFloat64Array() {
+      return new Float64Array([this.a, this.b, 0, 0, this.c, this.d, 0, 0, 0, 0, 1, 0, this.e, this.f, 0, 1]);
+    }
+  }
+
+  globalThis.DOMMatrix = DOMMatrixPolyfill;
+  globalThis.DOMMatrixReadOnly = DOMMatrixPolyfill;
+}
